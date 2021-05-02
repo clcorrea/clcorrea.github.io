253075682af016b9516d1d156dac29ff91b8ea2d
Chrystal Correa <clc03220@vsc.edu>
2021-05-02 13:21:12 -0400

    assignment9

---
+++ assignment9/script.js
@@ -0,0 +1,20 @@
+var isHidden = true;
+
+function toggleMain () {
+    var display;
+
+    if (isHidden) {
+        isHidden = false;
+        display = "block";
+    } else {
+        isHidden = true;
+        display = "none";
+    }
+    var content = document.getElementById ("content");
+    content.style.display = display;
+}
+var sections = document.getElementsByTagName ("section");
+
+for (var i = 0; i < sections.length; i++) {
+    sections [i].style.borderColor = "red";
+}
