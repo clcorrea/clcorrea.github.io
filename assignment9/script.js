e00e52a00f8939d04c3b7b46543f577af7d01604
Chrystal Correa <clc03220@vsc.edu>
2021-05-02 14:09:49 -0400

    Update script.js

--- assignment9/script.js
+++ assignment9/script.js
@@ -13,13 +13,14 @@ function toggleMain() {
     var content = document.getElementById("content");
     content.style.display = display;
 }
+
 var sections = document.getElementsByTagName("section");
 
 for (var i = 0; i < sections.length; i++) {
     sections[i].style.borderColor = "red";
 }
 
-/* The toggleMain function affects the display. 
-It allows the content with the main id
-to be hidden when clicking the title Vegetarian Pad Thai.
- */
+/* The toggleMain function affects the display of the page. 
+It allows the content with the main id to be hidden when 
+clicking the title Vegetarian Pad Thai (onclick="toggleMain()").
+The */
